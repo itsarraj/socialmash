a325e61fe2ef29244784768a9cd3a3614aed2e30
Animesh Raj <animesh080808@gmail.com>
2023-03-25 15:23:36 +0530

    Feat (UsersRoute) : Added User Routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 // inside home_controller we created a home that was exported there and accessed here with .home ;
+
+// any requests come to '/' or anything other than user appended to it will be forwarded to the homeController
 router.get('/', homeController.home);
-router.get('/contact', homeController.contact);
-router.get('/a', homeController.a);
-router.get('/b', homeController.b);
-router.get('/c', homeController.c);
-router.get('/d', homeController.d);
+
+router.use('/users', require('./users'));
+
+// for any further routes , access from here
+// router.use('/routerName', require('./routerFile'));
 
 module.exports = router;
