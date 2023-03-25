c5cea81e1d4b9ca272fc2386ac8d8e4b14747315
Animesh Raj <animesh080808@gmail.com>
2023-03-25 14:33:35 +0530

    Feat (controllers) : Controllers Setup Done

---
+++ controllers/home_controller.js
@@ -0,0 +1,22 @@
+module.exports.home = function (req, res) {
+    return res.end(`<h1>Express UP</h1>`);
+};
+
+module.exports.contact = function (req, res) {
+    return res.end(`<h1>contact</h1>`);
+};
+
+module.exports.a = function (req, res) {
+    return res.end(`<h1>a</h1>`);
+};
+module.exports.b = function (req, res) {
+    return res.end(`<h1>b</h1>`);
+};
+module.exports.c = function (req, res) {
+    return res.end(`<h1>c</h1>`);
+};
+module.exports.d = function (req, res) {
+    return res.end(`<h1>d</h1>`);
+};
+
+// module.exports.actionname = function (req, res) {}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+
 const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+// inside home_controller we created a home that was exported there and accessed here with .home ;
+router.get('/', homeController.home);
+router.get('/contact', homeController.contact);
+router.get('/a', homeController.a);
+router.get('/b', homeController.b);
+router.get('/c', homeController.c);
+router.get('/d', homeController.d);
 
 module.exports = router;
