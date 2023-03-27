3532206517295114f01019055ef5599c15e5cd8e
Animesh Raj <animesh080808@gmail.com>
2023-03-27 05:36:34 +0530

    Updated users_controller and user_profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,27 +1,18 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const app = express();
-app.use(cookieParser());
-
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    console.log(res.headers);
-    // let user_id_fetched = res.cookie.user_id;
-    // User.findOne({ id: req.body.id }, function (err, user) {});
+    console.log(req.cookies.user_id);
 
-    // User.findOne({ _id: { $eq: res.cookie.user_id } }, function (err, user) {
-    //     console.log(_id);
-    // });
-    // // if ((user.id == res.cookie.user_id)) {
-
-    // }
-
-    //
-    return res.render('user_profile', {
-        title: 'User Profile',
-        // email: req.body.email,
-        // name: req.body.name,
+    User.findById(req.cookies.user_id, function (err, user) {
+        if (user) {
+            return res.render('user_profile', {
+                title: 'User Profile',
+                user: user,
+            });
+        } else {
+            // Anyone can access cookies without actually being logged in .. so send them to sign in
+            return res.redirect('/users/sign-in');
+        }
     });
 };
 //  Render the signup page
