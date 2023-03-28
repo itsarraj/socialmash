d8b48aaab18b3598926a6a1c1791f08e7921e0a3
Animesh Raj <animesh080808@gmail.com>
2023-03-28 19:16:51 +0530

    Feat ( redirectProfile ): Setup a function in users_controller to redirect authenticated users to profile page if they try to open signin or signup page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,12 +7,19 @@ module.exports.profile = function (req, res) {
 };
 //  Render the signup page
 module.exports.signup = function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: 'socialmash | Xsignup',
     });
 };
 //  Render the signin page
 module.exports.signin = function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: 'socialmash | Xsignin',
     });
