e16714d066157b3476664c598ab58c879de98c18
Animesh Raj <animesh080808@gmail.com>
2023-04-01 05:12:55 +0530

    Feat : Deleting Post Feature to Authourised User

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createpost = async function (req, res) {
     try {
@@ -15,4 +16,19 @@ module.exports.createpost = async function (req, res) {
         }
         return res.redirect('back');
     }
+};
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+        // `_id` has to be converted to string to compare , but mongoose provides a way to do this with the `id` property
+        if (post.user == req.user.id) {
+            await Post.deleteMany({ _id: post._id });
+            await Comment.deleteMany({ post: req.params.id });
+            return res.redirect('back');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return res.redirect('back');
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+
 const postsController = require('../controllers/posts_controller');
 
 router.post(
     '/create-post',
     passport.checkAuthentication,
     postsController.createpost
+);
+
+router.get(
+    '/destroy/:id',
+    passport.checkAuthentication,
+    postsController.destroy
 );
 
 module.exports = router;
