2cee23b226c2ddf48e50c059c344e8db9334371c
Animesh Raj <animesh080808@gmail.com>
2023-05-11 16:12:43 +0530

    feat : setting up profiles
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Loader } from '../components/index';
 import { getPosts } from '../api/index';
+import { useAuth } from '../hooks';
+import styles from '../styles/home.module.css';
+
 const Home = () => {
+    const auth = useAuth();
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState([]);
 
@@ -20,9 +24,40 @@ const Home = () => {
         return <Loader />;
     }
     return (
-        <>
-            <div>Home</div>
-        </>
+        <div className={styles.container}>
+            <div className={styles.feedpost}>
+                <div className={styles.feedpostImg}>
+                    <img
+                        src="https://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Best-Quality-Profile-Images-Pic-Download-2023.jpg"
+                        alt="aa"
+                        srcset=""
+                    />
+                </div>
+                <div className={styles.inputArea}>
+                    <input type="text" />
+                </div>
+
+                <div className={styles.feedPostButton}>
+                    <button>send</button>
+                </div>
+            </div>
+            <div className={styles.feeds}>
+                <div className={styles.feedpostImg}>
+                    <img
+                        src="https://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Best-Quality-Profile-Images-Pic-Download-2023.jpg"
+                        alt="aa"
+                        srcset=""
+                    />
+                </div>
+                <div className={styles.inputArea}>
+                    <input type="text" />
+                </div>
+
+                <div className={styles.feedPostButton}>
+                    <button>send</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
